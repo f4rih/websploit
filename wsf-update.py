1bc15194dc9624cc917af2cf17bef81635bf0f05
websploit <0x0ptim0us@gmail.com>
2014-09-22 01:08:16 +0330

    fixed bugs

--- wsf-update.py
+++ wsf-update.py
@@ -9,9 +9,14 @@ import os
 from core import wcolors
 
 def update_websploit():
-	os.chdir("/tmp")
-	subprocess.Popen("mkdir test", shell=True)
+	print wcolors.color.GREEN + "[*]Updating Websploit framework, Please Wait ..." + wcolors.color.ENDC
+	os.chdir("/usr/share/")
+	try:
+		subprocess.Popen("git clone https://github.com/websploit/websploit.git", shell=True)
+	except Exception, e:
+		print wcolors.color.RED + "[!] Update Failed."+ wcolors.color.ENDC
+		pass
 
-
+	print wcolors.color.GREEN + "[*]Update was completed successfully." + wcolors.color.ENDC
 if __name__ == "__main__":
 	update_websploit()
